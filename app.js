ed3be7266b82ea5795fd050ef3b8f54813a333d1
Ann Huang <s52561@yahoo.com.tw>
2020-09-04 00:34:28 +0800

    add login register main pages

--- app.js
+++ app.js
@@ -12,9 +12,35 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+// 設定路由
+// 首頁
 app.get('/', (req, res) => {
   res.send('hello world')
 })
+
+// 登入註冊登出頁面
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
+
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
