fe4103da45a233514668f9d5ddf4daf33f5284f0
Ann Huang <s52561@yahoo.com.tw>
2020-09-04 14:39:52 +0800

    feat:add message partials

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
+const flash = require('connect-flash')
 // const bcrypt = require('bcryptjs')
 // / 引用 passport，放在文件上方
 // const passport = require('passport')
@@ -31,7 +32,14 @@ app.use(methodOverride('_method'))
 // const User = db.User
 
 usePassport(app)
-
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
 // 設定路由
 // 首頁
 // app.get('/', (req, res) => {
